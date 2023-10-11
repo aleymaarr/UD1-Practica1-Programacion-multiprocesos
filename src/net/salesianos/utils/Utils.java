2d2245d32d4c1080a139c08bfece0c22202706f2
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:30:44 +0100

    Feature: Creacion de la clase Utils y metodos getAllines el parametro que se le pasa es fileRoute

--- src/net/salesianos/utils/Utils.java
+++ src/net/salesianos/utils/Utils.java
@@ -1,4 +1,42 @@
 package net.salesianos.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Utils {
+    public static ArrayList<String> getAllLines(String fileRoute) {
+        ArrayList<String> lines = new ArrayList<>();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+
+            String currentLine = bufferedReader.readLine();
+
+            while (currentLine != null) {
+                lines.add(currentLine);
+                currentLine = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+
+    public static Integer getTotalWordsFrom(String fileRoute) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+            String firsLine = bufferedReader.readLine();
+            bufferedReader.close();
+            return Integer.parseInt(firsLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
 }
