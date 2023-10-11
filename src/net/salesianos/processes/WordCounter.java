18373c51519ced5086aa6ddc659697ad8d3da4ca
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:26:35 +0100

    Feature:Creacion de la clase WordCounter y creacion del metodo main

--- src/net/salesianos/processes/WordCounter.java
+++ src/net/salesianos/processes/WordCounter.java
@@ -1,4 +1,14 @@
 package net.salesianos.processes;
 
+
 public class WordCounter {
+    public static void main(String[] args) {
+        String line = args[0];
+        String[] words = line.split(" ");
+        System.out.println(words.length);
+
+        String line = args[0];
+        String[] words = line.split(" ");
+        System.out.println(words.length);
+    }
 }
