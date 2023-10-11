e61e5c9cc8ed2cfaba7ea847ea76a7aae8790bc8
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:06:06 +0100

    Feature:Creacion de la rama main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import net.salesianos.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<String> lines = Utils.getAllLines("/Users/alejandro/IdeaProjects/UD1-Practica1-Programacion-multiproceso/src/loremipsum.txt");
+        ArrayList<Process> allProcesses = new ArrayList<>();
+        String[] vocales = new String[] { "a", "e", "i", "o", "u" };
+        HashMap<String, Integer> vocalCounts = new HashMap<>();
 
     }
 }
