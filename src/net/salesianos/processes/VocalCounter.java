b36c43c50b0a2b3ee651a18883fbc052e9038f35
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:17:05 +0100

    Feature:Creando una intancia de ProcessBuilder y tambien las rutas de los archivos y utilizando Exepciones

--- src/net/salesianos/processes/VocalCounter.java
+++ src/net/salesianos/processes/VocalCounter.java
@@ -1,4 +1,16 @@
 package net.salesianos.processes;
 
 public class VocalCounter {
+    public static void main(String[] args) {
+        String vocal = args[0];
+        String line = args[1];
+        int count = 0;
+        for(int i = 0; i < line.length(); i++) {
+            if(line.charAt(i) == vocal.charAt(0)) {
+                count++;
+            }
+        }
+        System.out.println(vocal + "," + count);
+    }
 }
+
