0eda9e50ea45cee10f5daef928eb17911076af4d
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:08:33 +0100

    Feature:Creando for para contar las vocales,lineas,procesos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import net.salesianos.launcher.ProcessLauncher;
 import net.salesianos.utils.Utils;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,6 +13,75 @@ public class Main {
         ArrayList<Process> allProcesses = new ArrayList<>();
         String[] vocales = new String[] { "a", "e", "i", "o", "u" };
         HashMap<String, Integer> vocalCounts = new HashMap<>();
+
+        for (String vocal : vocales) {
+            vocalCounts.put(vocal, 0);
+        }
+
+        for (String line : lines) {
+            for (String vocal : vocales) {
+                Process javaProcess = ProcessLauncher.initContadorVocalProcess(vocal, line);
+                allProcesses.add(javaProcess);
+            }
+        }
+
+        for (String line : lines) {
+            for (String vocal : vocales) {
+                Process javaProcess = ProcessLauncher.initContadorVocalProcess(vocal, line);
+                allProcesses.add(javaProcess);
+            }
+        }
+
+        for (Process process : allProcesses) {
+            try {
+                process.waitFor();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String output = reader.readLine();
+
+                if (output != null) {
+                    String[] parts = output.split(",");
+                    if (parts.length >= 2) {
+                        String vocal = parts[0];
+                        int count = Integer.parseInt(parts[1]);
+                        vocalCounts.put(vocal, vocalCounts.get(vocal) + count);
+                    } else {
+                        System.out.println("Formato de salida inesperado: " + output);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Process process : allProcesses) {
+            try {
+                process.waitFor();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String output = reader.readLine();
+
+                if (output != null) {
+                    String[] parts = output.split(",");
+                    if (parts.length >= 2) {
+                        String vocal = parts[0];
+                        int count = Integer.parseInt(parts[1]);
+                        vocalCounts.put(vocal, vocalCounts.get(vocal) + count);
+                    } else {
+                        System.out.println("Formato de salida inesperado: " + output);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        int totalCount = 0;
+        for (String vocal : vocales) {
+            int count = vocalCounts.get(vocal);
+            System.out.println("Total de " + vocal + ": " + count);
+            totalCount += count;
+        }
+        System.out.println("Total de vocales: " + totalCount);
+    }
 
     }
 }
