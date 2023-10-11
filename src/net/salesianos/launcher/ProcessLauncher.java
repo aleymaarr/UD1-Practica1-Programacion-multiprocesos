a7a6bca9ff4f67af955f7f4cfb43f28969d418ba
aleymarr <aleyba2002@gmail.com>
2023-10-11 13:14:14 +0100

    Feature:Crear un metodo initcontadorVocalProces que se le pasa por parametros vocal,line

--- src/net/salesianos/launcher/ProcessLauncher.java
+++ src/net/salesianos/launcher/ProcessLauncher.java
@@ -1,4 +1,22 @@
 package net.salesianos.launcher;
 
+import java.io.IOException;
+
 public class ProcessLauncher {
+
+    public static Process initContadorVocalProcess(String vocal, String line) {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "/Users/alejandro/IdeaProjects/UD1-Practica1-Programacion-multiproceso/src/net/salesianos/processes/VocalCounter.java", vocal, line
+        );
+
+        Process javaProcess = null;
+
+        try {
+            javaProcess = processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return javaProcess;
+    }
 }
